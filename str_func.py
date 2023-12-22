62e3a7ecb530c450e5fecf58f9868018722a4424
Korolev Aleksandr <sanyastronger@rambler.ru>
2023-12-22 16:47:17 +0300

    добавил локально одну функцию  и два докстрина

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def get_upper_str(data):
+    """
+    делает заглавными все буквы
+    """
     return data.upper()
+
+
+def get_first_str_up(data):
+    """
+    делает первую букву заглавной
+    """
+    return data.title()
